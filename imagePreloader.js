da50823471f515fee717eb5f1efd8267a4a464c2
Sam Eaton <sameaton11@gmail.com>
2015-03-26 23:55:04 -0600

    Wrap in anonymous function
    Wrap in anonymous function so there are no global variables or possible scope conflicts.

--- imagePreloader.js
+++ imagePreloader.js
@@ -1,23 +1,25 @@
-var imgsToLoad = [
-  "path/to/img1.png",
-  "path/to/img2.png",
-  "path/to/img3.jpg",
-  "path/to/img4.png",
-  "path/to/img5.png",
-  "path/to/img6.jpg",
-];
-var loadedImgs = [];
-var i = 0;
-function imgPreloader() {
-	loadedImgs[i] = new Image();
-	loadedImgs[i].src = imgsToLoad[i];
-	loadedImgs[i].onload = function(){
-		if(imgsToLoad[i+1]){
-			// console.log("loaded: "+loadedImgs[i].src); // UN-COMMENT FOR CONSOLE OUTPUT
-			i++;  imgPreloader();
-		}
-	};
-}
-window.addEventListener('load',function(){
-		imgPreloader();
-});
+(function(){
+	var imgsToLoad = [
+		"path/to/img1.png",
+	  	"path/to/img2.png",
+	  	"path/to/img3.jpg",
+	  	"path/to/img4.png",
+	  	"path/to/img5.png",
+	  	"path/to/img6.jpg",
+	];
+	var loadedImgs = [];
+	var i = 0;
+	function imgPreloader() {
+		loadedImgs[i] = new Image();
+		loadedImgs[i].src = imgsToLoad[i];
+		loadedImgs[i].onload = function(){
+			if(imgsToLoad[i+1]){
+				// console.log("loaded: "+loadedImgs[i].src); // UN-COMMENT FOR CONSOLE OUTPUT
+				i++;  imgPreloader();
+			}
+		};
+	}
+	window.addEventListener('load',function(){
+			imgPreloader();
+	});
+})();
